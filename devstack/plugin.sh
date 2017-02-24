9a12f7dee695b55f7f69df4cfedab517fdfab221
YAMAMOTO Takashi <yamamoto@midokura.com>
2017-02-23 22:32:32 -0500

    devstack: Adapt to lib/neutron
    Depends-On: I9068fd608e82e70db8d725f92269a26920efebcb
    Change-Id: Idcb903002aec823aa1e546e8e90815ebac614e5b

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,5 +1,12 @@
 LIBDIR=$DEST/neutron/devstack/lib
 
+if is_neutron_legacy_enabled; then
+    NEUTRON_CORE_PLUGIN=$Q_PLUGIN
+    NEUTRON_AGENT=$Q_AGENT
+    NEUTRON_CORE_PLUGIN_CONF_PATH=$Q_PLUGIN_CONF_PATH
+    NEUTRON_CORE_PLUGIN_CONF=$Q_PLUGIN_CONF_FILE
+fi
+
 source $LIBDIR/dns
 source $LIBDIR/flavors
 source $LIBDIR/l2_agent
@@ -11,26 +18,17 @@ source $LIBDIR/trunk
 
 Q_BUILD_OVS_FROM_GIT=$(trueorfalse False Q_BUILD_OVS_FROM_GIT)
 
-if [ -f $LIBDIR/${Q_AGENT}_agent ]; then
-    source $LIBDIR/${Q_AGENT}_agent
+if [ -f $LIBDIR/${NEUTRON_AGENT}_agent ]; then
+    source $LIBDIR/${NEUTRON_AGENT}_agent
 fi
 
 if [[ "$1" == "stack" ]]; then
     case "$2" in
         install)
-            if is_service_enabled q-flavors; then
-                configure_flavors
-            fi
-            if is_service_enabled q-qos; then
-                configure_qos
-            fi
-            if is_service_enabled q-trunk; then
-                configure_trunk_extension
-            fi
-            if is_service_enabled q-dns; then
+            if is_service_enabled q-dns neutron-dns; then
                 configure_dns_extension
             fi
-            if [[ "$Q_AGENT" == "openvswitch" ]] && \
+            if [[ "$NEUTRON_AGENT" == "openvswitch" ]] && \
                [[ "$Q_BUILD_OVS_FROM_GIT" == "True" ]]; then
                 remove_ovs_packages
                 compile_ovs True /usr /var
@@ -38,10 +36,19 @@ if [[ "$1" == "stack" ]]; then
             fi
             ;;
         post-config)
-            if is_service_enabled q-dns; then
+            if is_service_enabled q-flavors neutron-flavors; then
+                configure_flavors
+            fi
+            if is_service_enabled q-qos neutron-qos; then
+                configure_qos
+            fi
+            if is_service_enabled q-trunk neutron-trunk; then
+                configure_trunk_extension
+            fi
+            if is_service_enabled q-dns neutron-dns; then
                 post_config_dns_extension
             fi
-            if is_service_enabled q-agt; then
+            if is_service_enabled q-agt neutron-agent; then
                 configure_l2_agent
             fi
             #Note: sriov agent should run with OVS or linux bridge agent
@@ -49,20 +56,21 @@ if [[ "$1" == "stack" ]]; then
             #Currently devstack lacks the option to run two agents on the same node.
             #Therefore we create new service, q-sriov-agt, and the q-agt should be OVS
             #or linux bridge.
-            if is_service_enabled q-sriov-agt; then
-                configure_$Q_PLUGIN
+            if is_service_enabled q-sriov-agt neutron-sriov-agent; then
+                configure_$NEUTRON_CORE_PLUGIN
                 configure_l2_agent
                 configure_l2_agent_sriovnicswitch
             fi
+            configure_ml2_extension_drivers
             ;;
         extra)
-            if is_service_enabled q-sriov-agt; then
+            if is_service_enabled q-sriov-agt neutron-sriov-agent; then
                 start_l2_agent_sriov
             fi
             ;;
     esac
 elif [[ "$1" == "unstack" ]]; then
-    if is_service_enabled q-sriov-agt; then
+    if is_service_enabled q-sriov-agt neutron-sriov-agent; then
         stop_l2_agent_sriov
     fi
 fi
